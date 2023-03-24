5262d3085b2160ed63eda4e03cd9ee2b8de596af
Andriy Demeshko <ande777complaint@gmail.com>
2023-03-24 15:52:29 +0200

    Create views for task list and tags

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Tag, Task
 
-# Create your views here.
+
+class TagListView(generic.ListView):
+    model = Tag
+    context_object_name = "tag_list"
+    template_name = "todo/tag_list.html"
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    context_object_name = "task_list"
+    template_name = "todo/task_list.html"
+
